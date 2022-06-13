b9c68d3d8eb79d6fb789718f2b8e40d1c58f1578
Md. Hasan Ragib Ullah <ragibhasan894@gmail.com>
2022-06-13 23:28:33 +0600

    solved time conversion

--- TimeConversion.js
+++ TimeConversion.js
@@ -22,10 +22,6 @@ function readLine() {
     return inputString[currentLine++];
 }
 
-function tsddd(r) {
-    console.log(r);
-}
-
 /*
  * Complete the 'timeConversion' function below.
  *
@@ -33,8 +29,34 @@ function tsddd(r) {
  * The function accepts STRING s as parameter.
  */
 
-function timeConversion(s) {
-    tsddd(s);
+function timeConversion(tms) {
+    tms = tms.toString();
+
+    let changedHour = '';
+
+    let lastprt = tms.slice(-2);
+
+    let timeBrkDown = tms.split(':');
+
+    let hour = parseInt(timeBrkDown[0]);
+    let mint = timeBrkDown[1];
+    let secnd = timeBrkDown[2].substring(0,2);
+
+    if (lastprt == 'AM' && hour == 12) {
+        changedHour = '00';
+    } else if (lastprt == 'AM' && (hour >= 1 && hour <= 11)) {
+        changedHour = hour.toString();
+    } else if (lastprt == 'PM' && hour == 12) {
+        changedHour = hour.toString();
+    } else if (lastprt == 'PM' && (hour >= 1 && hour < 12)) {
+        changedHour = (12 + hour).toString();
+    }
+
+    if (changedHour.length < 2) {
+        changedHour = '0' + changedHour;
+    }
+
+    return (changedHour + ':' + mint + ':' + secnd);
 }
 
 function main() {
--- test.js
+++ test.js
@@ -1,6 +1,28 @@
 
-let date = process.argv;
+let tms = process.argv.slice(2);
 
+tms = tms[0];
 
+let lastprt = tms.slice(-2);
 
-console.log(date);
+let timeBrkDown = tms.split(':');
+
+let hour = parseInt(timeBrkDown[0]);
+let mint = timeBrkDown[1];
+let secnd = timeBrkDown[2].substring(0,2);
+
+if (lastprt == 'AM' && hour == 12) {
+    changedHour = '00';
+} else if (lastprt == 'AM' && (hour >= 1 && hour <= 11)) {
+    changedHour = hour.toString();
+} else if (lastprt == 'PM' && hour == 12) {
+    changedHour = hour.toString();
+} else if (lastprt == 'PM' && (hour >= 1 && hour < 12)) {
+    changedHour = (12 + hour).toString();
+}
+
+if (changedHour.length < 2) {
+    changedHour = '0' + changedHour;
+}
+
+console.log(changedHour + ':' + mint + ':' + secnd);
